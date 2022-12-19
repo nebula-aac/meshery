ce0542b27038660e29ae62ebbee0b2ab743606ed
Antonette Caldwell <pullmana8@gmail.com>
2022-12-19 13:05:44 -0600

    Removed deprecated eslint packages, and reconfigured eslint to extend next plugin
    Signed-off-by: Antonette Caldwell <pullmana8@gmail.com>

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -8,7 +8,8 @@ module.exports = {
   "extends" : [
     "eslint:recommended",
     "plugin:react/recommended",
-    "plugin:cypress/recommended"
+    "plugin:cypress/recommended",
+    "plugin:@next/next/recommended"
   ],
   "globals" : {
     "Atomics" : "readonly",
