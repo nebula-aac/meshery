94f7aa28d44c2687f63dfc3875491affc5826836
Antonette Caldwell <acald.nebula@gmail.com>
2023-06-12 13:44:29 -0500

    Setup MUI v5

--- ui/pages/_app.js
+++ ui/pages/_app.js
@@ -1,5 +1,21 @@
-import '@/styles/globals.css'
+import theme from "@/styles/theme";
+import createEmotionCache from "@/utils/createEmotionCache";
+import { CacheProvider } from "@emotion/react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Head from "next/head";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const clientSideEmotionCache = createEmotionCache();
+
+export default function App({ Component, emotionCache = clientSideEmotionCache, pageProps }) {
+  return (
+    <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
